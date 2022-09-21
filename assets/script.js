85ace0d4fe37cfac5978cb3f270585db0539896d
Grahambone37 <electricgrunt37@gmail.com>
2022-09-20 20:00:18 -0700

    remove button working

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
-//variables
+//minus and plus variables
 let quantityDown = document.querySelector('#quantity-down')
 let quantityUp = document.querySelector('#quantity-up')
 let totalQuantity = document.querySelector('#quantity-number')
 let quantity = 0
+//remove variables
+let removeButton = document.querySelector('.remove')
+let cartItem = document.querySelector('.cart-item')
 //minus
     quantityDown.addEventListener('click', function(){
     if (quantity >= 1){
@@ -17,3 +20,8 @@ quantityUp.addEventListener('click', function(){
     quantity += 1
     totalQuantity.textContent = `${quantity}`
 })
+//remove working
+    removeButton.addEventListener('click', function(){
+        cartItem.remove()
+    })
+    
